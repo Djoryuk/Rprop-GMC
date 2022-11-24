5424a5eecf4b5eba9f5337571ee2d838ec9b59f0
Djo <abdeldjallil-boudekhana@outlook.fr>
2022-11-23 21:24:18 -0800

    Fixing PropType

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import IMG from "./Asset/photo.jpg";
 function App({handleName}) {
   return (
     <>
-    {/* The profile component gets data (fullName, bio, profession) as props */}
     <Profile 
       fullName="Abdeldjallil boudekhana " 
       bio="Hey ! i am abdel djallil im learning Full sztack web developpment " 
       profession="FullStack Web Developper" 
-
-      /*gets the function handleName functions as a prop that sends an alert message with the name of the profile user. */
       onMouseOver={handleName}
     >
-      {/*gets image as children props*/}
       {IMG}
     </Profile>
   </>
